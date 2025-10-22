776882d9754c4c8d5b1ec7ac63fc9a469e09bb62
Shubham Thakur <shubham.codecreator@gmail.com>
2025-10-22 19:35:14 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,9 @@ app.get('/', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
+if(process.env.NODE_ENV !== "production"){ //condition for localhost
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+}
